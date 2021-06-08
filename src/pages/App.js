8d56874ee1e7cb58186c04ed10674144c96b8b7b
Lyderies <lyderies@hotmail.com>
2021-06-08 16:55:03 +1200

    Routes Set up, tested, working

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,41 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Home from "./Home";
+import Tim from "./Tim";
+import Lucian from "./Lucian";
+
 function App() {
   return (
-    <div className="bg-black h-screen w-screen">
-      <header className=""></header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Lucian">Lucian</Link>
+            </li>
+            <li>
+              <Link to="/Tim">Tim</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/Tim">
+            <Tim />
+          </Route>
+          <Route path="/Lucian">
+            <Lucian />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Tim.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tim() {
+  return <div></div>;
+}
+
+export default Tim;
