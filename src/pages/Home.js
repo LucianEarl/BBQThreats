43ba6df21c8fc5a8f64e382c8b08964f57bbbb68
Lyderies <lyderies@hotmail.com>
2021-06-09 14:49:44 +1200

    Home buttom base implementation and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+// Home background components
 import Background from "../components/background/Background";
 import BackgroundData from "../components/background/BackgroundData";
+
+// Home Button
+import Button from "../components/home/HomeButton";
+// Picture of Lucian and Tim
+import lucianPic from "../media/images/lucian.jpg";
+import timPic from "../media/images/tim.jpg";
 
 function Home() {
   return (
@@ -20,8 +27,10 @@ function Home() {
         <article className="text-3xl p-2">
           <h2>Who?</h2>
         </article>
-        <article className="">Lucian goes here</article>
-        <article className="">Tim goes here</article>
+        <article className="container flex flex-row">
+          <Button name={"Lucian Earl"} image={lucianPic} />
+          <Button name={"Tim Whatley"} image={timPic} />
+        </article>
       </section>
       <Background BackgroundData={BackgroundData[0]} />
     </div>
